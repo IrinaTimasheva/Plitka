b0322916cdd5c1a223b50efb784c1b63d6e28cc6
Ирина Тимашева <62283497+IrinaTimasheva@users.noreply.github.com>
2020-04-28 16:30:50 +0400

    Добавьте файлы проекта.

---
+++ Plitka/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plitka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Svesda_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Lapka_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Oblako_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Karandash_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void List_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Player_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+
+        private void Klava_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, раздел находится в разработке");
+        }
+    }
+}
